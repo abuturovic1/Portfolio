b090c00599080269b950932257856dc68facfeed
Amina <abuturovic1@etf.unsa.ba>
2024-05-25 23:26:47 +0200

    added tech stack list div

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,6 +1,13 @@
+// import { cn } from "@/utils/cn";
+// import { BackgroundGradientAnimation } from "./GradientBg";
+// import GridGlobe from "./GridGlobe";
+'use client'
 import { cn } from "@/utils/cn";
 import { BackgroundGradientAnimation } from "./GradientBg";
 import GridGlobe from "./GridGlobe";
+import { useState } from "react";
+import animationData from '@/data/confetti.json';
+import Lottie from "react-lottie";
 
 export const BentoGrid = ({
     className,
@@ -12,7 +19,8 @@ export const BentoGrid = ({
     return (
         <div
             className={cn(
-                "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ",
+
+                "grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto",
                 className
             )}
         >
@@ -22,12 +30,11 @@ export const BentoGrid = ({
 };
 
 export const BentoGridItem = ({
-    //     
     className,
     id,
     title,
     description,
-    //   remove unecessary things here
+
     img,
     imgClassName,
     titleClassName,
@@ -42,19 +49,41 @@ export const BentoGridItem = ({
     titleClassName?: string;
     spareImg?: string;
 }) => {
+    const leftLists = ["ReactJS", "Express", "Typescript"];
+    const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+
+    const [copied, setCopied] = useState(false);
+
+    const defaultOptions = {
+        loop: copied,
+        autoplay: copied,
+        animationData: animationData,
+        rendererSettings: {
+            preserveAspectRatio: "xMidYMid slice",
+        },
+    };
+
+    const handleCopy = () => {
+        const text = "hsu@jsmastery.pro";
+        navigator.clipboard.writeText(text);
+        setCopied(true);
+    };
 
     return (
         <div
             className={cn(
+
                 "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
                 className
             )}
             style={{
+
                 background: "rgb(4,7,29)",
                 backgroundColor:
                     "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
             }}
         >
+
             <div className={`${id === 6 && "flex justify-center"} h-full`}>
                 <div className="w-full h-full absolute">
                     {img && (
@@ -73,20 +102,17 @@ export const BentoGridItem = ({
                         <img
                             src={spareImg}
                             alt={spareImg}
+
                             className="object-cover object-center w-full h-full"
                         />
                     )}
                 </div>
-                {id==6 && (
+                {id === 6 && (
+
                     <BackgroundGradientAnimation>
-                        <div className="absolute z-50 flex items-center justify-center text-white font-bold"/>
+                        <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div>
                     </BackgroundGradientAnimation>
                 )}
-                {/* <div className={cn(
-                    titleClassName, 'group-hover'
-                )}>
-
-                </div> */}
 
                 <div
                     className={cn(
@@ -100,11 +126,64 @@ export const BentoGridItem = ({
                     </div>
 
                     <div
-                        className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
+                        className={`font-sans text-lg lg:text-3xl max-w-96 font-bold z-10`}
+                    >
                         {title}
                     </div>
+
+                    {id === 2 && <GridGlobe />}
+
+
+                    {id === 3 && (
+                        <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+
+                            <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
+                                {leftLists.map((item, i) => (
+                                    <span
+                                        key={i}
+                                        className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                                    >
+                                        {item}
+                                    </span>
+                                ))}
+                                <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span>
+                            </div>
+                            <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
+                                <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span>
+                                {rightLists.map((item, i) => (
+                                    <span
+                                        key={i}
+                                        className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                                    >
+                                        {item}
+                                    </span>
+                                ))}
+                            </div>
+                        </div>
+                    )}
+                    {id === 6 && (
+                        <div className="mt-5 relative">
+
+                            <div
+                                className={`absolute -bottom-5 right-0 ${copied ? "block" : "block"
+                                    }`}
+                            >
+
+                                <Lottie options={defaultOptions} height={200} width={400} />
+                            </div>
+
+                            {/* <MagicButton
+                title={copied ? "Email is Copied!" : "Copy my email address"}
+                icon={<IoCopyOutline />}
+                position="left"
+                handleClick={handleCopy}
+                otherClasses="!bg-[#161A31]"
+              /> */}
+                        </div>
+                    )}
                 </div>
-                {id==2 && <GridGlobe/>} 
             </div>
         </div>
     );
