f8060684ac7c97f1ef537bd40536a579e29b0026
Amina <abuturovic1@etf.unsa.ba>
2024-05-26 12:55:33 +0200

    added my projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,49 @@
+// import Hero from "@/components/Hero";
+// import Grid from "@/components/Grid";
+// import { FloatingNav } from "@/components/ui/FloatingNav";
+// import { FaHome } from "react-icons/fa";
+// import Experience from "@/components/Experience";
+
+// export default function Home() {
+//   return (
+
+//     <main className="relative bg-black-100 flex justify-center items-center flex-col 
+//     min-h-screen overflow-hidden mx-auto sm:px-10 px-5">
+//       <div className="max-w-7xl w-full">
+//         <FloatingNav navItems={[
+//           { name: 'Home', link: '/', icon: <FaHome /> }
+//         ]} />
+//         <Hero />
+//         <Grid />
+//         <Experience />
+//       </div>
+//     </main>
+//   );
+// }
+"use client";
+
+import { navItems } from "@/data";
+
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
-import { FloatingNav } from "@/components/ui/FloatingNav";
-import { FaHome } from "react-icons/fa";
 import Experience from "@/components/Experience";
+// import { FloatingNav } from "@/components/ui/FloatingNav";
+import RecentProjects from "@/components/RecentProjects";
 
-export default function Home() {
+
+const Home = () => {
   return (
-    // min-h-screen
-    <main className="relative bg-black-100 flex justify-center items-center flex-col 
-    min-h-screen overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={[
-          {name: 'Home', link: '/', icon: <FaHome />}
-        ]}  />
+      {/* <FloatingNav navItems={navItems} /> */}
         <Hero />
         <Grid />
+     <RecentProjects/>
         <Experience />
+       
       </div>
     </main>
   );
-}
+};
+
+export default Home;
--- data/index.ts
+++ data/index.ts
@@ -1,8 +1,8 @@
 export const navItems = [
   { name: "About", link: "#about" },
   { name: "Projects", link: "#projects" },
-  { name: "Testimonials", link: "#testimonials" },
-  { name: "Contact", link: "#contact" },
+  // { name: "Testimonials", link: "#testimonials" },
+  // { name: "Contact", link: "#contact" },
 ];
 
 export const gridItems = [
@@ -49,12 +49,12 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently building a JS Animation library",
+    title: "Currently building a VR training app",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
-    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    imgClassName: "absolute right-0 bottom-0 md:w-94 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
-    img: "/b5.svg",
+    img: "/Vrapp.jpg",
     spareImg: "/grid.svg",
   },
   {
@@ -72,36 +72,35 @@ export const gridItems = [
 export const projects = [
   {
     id: 1,
-    title: "3D Solar System Planets to Explore",
-    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-    img: "/p1.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-    link: "https://github.com/adrianhajdin?tab=repositories",
+    title: "SkinCarePro",
+    des: "",
+    img: "/SkinCarePhoto.png",
+    iconLists: ["/Java_programming_language_logo.svg.png","/png-transparent-javafx-scene-builder-fxml-jar-mobile-app-development-builder-food-user-interface-design-orange-thumbnail.png","/CSS3_logo_and_wordmark.svg.png"],
+    link: "https://github.com/abuturovic1/SkinCarePro",
   },
   {
     id: 2,
-    title: "Yoom - Video Conferencing App",
-    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-    img: "/p2.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-    link: "https://github.com/adrianhajdin/zoom-clone",
+    title: "RealEstateApp",
+    des: "",
+    iconLists: ["/JavaScript-logo.png", "/HTML5_logo_and_wordmark.svg.png","/CSS3_logo_and_wordmark.svg.png"],
+    link: "https://github.com/abuturovic1/RealEstateApp",
   },
   {
     id: 3,
-    title: "AI Image SaaS - Canva Application",
-    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-    img: "/p3.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-    link: "https://github.com/adrianhajdin/ai_saas_app",
+    title: "SarajevoEvents",
+    des: "",
+    img: "/SaEvents.PNG",
+    iconLists: ["/c--4.svg","/JavaScript-logo.png", "/HTML5_logo_and_wordmark.svg.png","/CSS3_logo_and_wordmark.svg.png"],
+    link: "https://github.com/ooad-2021-2022/SarajevoEvents",
   },
-  {
-    id: 4,
-    title: "Animated Apple Iphone 3D Website",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-    img: "/p4.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-    link: "https://github.com/adrianhajdin/iphone",
-  },
+  // {
+  //   id: 4,
+  //   title: "Animated Apple Iphone 3D Website",
+  //   des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+  //   img: "/p4.svg",
+  //   iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+  //   link: "https://github.com/adrianhajdin/iphone",
+  // },
 ];
 
 export const testimonials = [
@@ -181,7 +180,7 @@ export const workExperience = [
   {
     id: 2,
     title: "Faculty Of Electrical Engineering, University of Sarajevo",
-    desc: "Computer Science and Informatics",
+    desc: "Department for Computer Science and Informatics",
     className: "md:col-span-2", 
     thumbnail: "/exp2.svg",
   }
